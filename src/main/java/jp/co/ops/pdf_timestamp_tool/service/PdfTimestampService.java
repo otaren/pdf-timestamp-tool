bed5e3661f1515f2b29f9ec7561b2f93835a82c8
otaren <150455192+otaren@users.noreply.github.com>
2025-08-07 22:52:56 +0900

    Update PdfTimestampService.java

--- src/main/java/jp/co/ops/pdf_timestamp_tool/service/PdfTimestampService.java
+++ src/main/java/jp/co/ops/pdf_timestamp_tool/service/PdfTimestampService.java
@@ -36,17 +36,20 @@ public class PdfTimestampService {
 		PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
 
 		PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
-
-		//　署名のメタ情報を設定（必須ではないため削除可能）
+		
+		// 署名のメタ情報を設定（必須ではないため削除可能）
 		appearance.setReason("電子署名証明");
 		appearance.setLocation("OPS");
-
+		
 		// タイムスタンプ署名を見えるように設定
-		appearance.setVisibleSignature(new Rectangle(100, 100, 250, 150), 1, "InvisibleSignature");
+		//appearance.setVisibleSignature(new Rectangle(100, 100, 250, 150), 1, "VisibleSignature");
+		
+		// 今回は見えないように設定
+		appearance.setVisibleSignature(new Rectangle(0, 0, 0, 0), 1, "InvisibleSignature");
 
 		// タイムスタンプのクライアントを設定
 		TSAClientBouncyCastle tsaClient = new TSAClientBouncyCastle(tsaUrl);
-
+		
 		// 外署名コンテナの作成
 		ExternalSignatureContainer external = new TSAContainer(tsaClient);
 
